2db1710f44fed1870ae73a2133fe4f9a1460e409
Hewllpark <hewllpark@gmail.com>
2023-11-17 11:55:22 +0900

    first commit

---
+++ src/main/java/com/sparta/preproject/PreProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.preproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreProjectApplication.class, args);
+    }
+
+}
